7c5b59ccc25f5025045a09045c9ab352dba90759
Paul <mppaul.23.5@gmail.com>
2025-03-16 19:34:35 -0300

    se agrego la funcionalidad agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+
+function agregarAmigo(){
+    let amigo = document.getElementById("amigo");
+
+if (amigo.value != ""){
+    amigos.push(amigo.value);
+    amigo.value="";
+    console.log(amigos);
+}
+else{
+    alert("Por favor, inserte un nombre.")
+}
+}
