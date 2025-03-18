c91340415663ae2f98d92ca7ce9d14eee3f0fe6d
paul-23-5 <mppaul.23.5@gmail.com>
2025-03-18 01:14:58 -0300

    Se implementó una función  para actualizar amigos

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
+let amigos = [];
 
+function agregarAmigo() {
+  let amigo = document.getElementById("amigo");
 
-function agregarAmigo(){
-    let amigo = document.getElementById("amigo");
-
-if (amigo.value != ""){
+  if (amigo.value != "") {
     amigos.push(amigo.value);
-    amigo.value="";
+    amigo.value = "";
     console.log(amigos);
+    actualizarAmigos();
+  } else {
+    alert("Por favor, inserte un nombre.");
+  }
 }
-else{
-    alert("Por favor, inserte un nombre.")
-}
+
+function actualizarAmigos() {
+  let lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  for (const amigo of amigos) {
+    nuevoElemento = document.createElement("li");
+    console.log(nuevoElemento);
+    nuevoElemento.innerHTML = amigo;
+    console.log(amigo);
+    lista.appendChild(nuevoElemento);
+  }
 }
